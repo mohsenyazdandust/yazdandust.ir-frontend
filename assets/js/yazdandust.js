af0bca739904e3f68defa6db912a4c7d0745ddd2
Mohsen Yazdandust <m.yazdandust1403@gmail.com>
2025-07-13 00:13:27 +0330

    inital commit

---
+++ assets/js/yazdandust.js
@@ -0,0 +1,4 @@
+// let colors = ['green', '#b80000', 'yellow', 'pink'];
+// let color_index = Math.floor(Math.random() * colors.length);
+// let color = '#'+(Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0');
+// document.documentElement.style.setProperty('--primary-color', color);
